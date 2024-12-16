74508a23754d4aef11fb06dda277728e38b4e919
Lorenzo Menghini <lollomenghini@gmail.com>
2024-12-16 14:20:52 +0100

    ops

--- lib/service/network/dio/dio_provider.dart
+++ lib/service/network/dio/dio_provider.dart
@@ -1,6 +1,3 @@
-import 'package:alice/alice.dart';
-import 'package:alice/model/alice_configuration.dart';
-import 'package:birex/service/routing/router.dart';
 import 'package:curl_logger_dio_interceptor/curl_logger_dio_interceptor.dart';
 import 'package:dio/dio.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,17 +9,9 @@ part 'dio_provider.g.dart';
 @riverpod
 Dio dio(Ref ref) {
   final dio = Dio();
-  final navKey = ref.watch(navigatorKeyProvider);
-  final alice = Alice(
-    configuration: AliceConfiguration(
-      navigatorKey: navKey,
-      showNotification: false,
-    ),
-  );
   final inteceptors = [
     CurlLoggerDioInterceptor(printOnSuccess: false),
     PrettyDioLogger(requestBody: true, requestHeader: true),
-    AliceI,
   ];
   dio.interceptors.addAll(inteceptors);
   return dio;
